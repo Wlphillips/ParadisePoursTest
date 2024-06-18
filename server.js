11d9027dffe433d6bb62862d607b72a6d2175d55
Almenorca <105606096+Almenorca@users.noreply.github.com>
2024-06-18 13:02:48 -0400

    Updated server.js
    Included Login API using the example from the Cards MERN lab.

--- server.js
+++ server.js
@@ -35,5 +35,25 @@ app.post('/api/register', async (req, res, next) => {
     const user = {UserId, FirstName, LastName, Username, Password, Email, Phone}
     await db.collection('Users').insertOne(user);
 })
+//User Login
+app.post('/api/login', async (req, res, next) => {
+    const db = client.db('AlcoholDatabase')
+    var error = ''
+    const { Username, Password } = req.body
+    const results = await
+    db.collection('Users').find({ Username:Username,Password:Password}).toArray()
+    var id = -1
+    var fn = ''
+    var ln = ''
+
+    if( results.length > 0 ) {
+        id = results[0].UserId
+        fn = results[0].FirstName
+        ln = results[0].LastName
+    }
+    
+    var ret = { id:id, firstName:fn, lastName:ln, error:''}
+    res.status(200).json(ret)
+})
 
 app.listen(5000); // Node + Express server will be on port 5000
