aec7076656841118fe01a196a0acd1beeb4d1dd9
Almenorca <105606096+Almenorca@users.noreply.github.com>
2024-06-19 13:48:05 -0400

    Updated server.js
    Cleaned up the server.js by separating database and APIs into separate files

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const {connectDatabase, closeDatabase} = require('./database');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-
-const MongoClient = require('mongodb').MongoClient;
-const url =
-'mongodb+srv://wlphillips:Admin@cluster0.tjtuhkp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-const client = new MongoClient(url);
-client.connect();
 
 //CORS Middleware
 app.use((req, res, next) => 
@@ -27,33 +22,11 @@ app.use((req, res, next) =>
   next();
 });
 
-//API Section
-//User Register
-app.post('/api/register', async (req, res, next) => {
-    const db = client.db('AlcoholDatabase');
-    const {UserId, FirstName, LastName, Username, Password, Email, Phone} = req.body
-    const user = {UserId, FirstName, LastName, Username, Password, Email, Phone}
-    await db.collection('Users').insertOne(user);
-})
-//User Login
-app.post('/api/login', async (req, res, next) => {
-    const db = client.db('AlcoholDatabase')
-    var error = ''
-    const { Username, Password } = req.body
-    const results = await
-    db.collection('Users').find({ Username:Username,Password:Password}).toArray()
-    var id = -1
-    var fn = ''
-    var ln = ''
+//Wrapper that connects to MongDB using connectDatabase() import
+connectDatabase().then(() => {
+  //API Section
+  const userAPI = require('./API/user');
+  app.use('/api', userAPI); //To access user APIs, you would need to add '/api' before any route (i.e. /api/login)
 
-    if( results.length > 0 ) {
-        id = results[0].UserId
-        fn = results[0].FirstName
-        ln = results[0].LastName
-    }
-    
-    var ret = { id:id, firstName:fn, lastName:ln, error:''}
-    res.status(200).json(ret)
+  app.listen(5000); //Starts server on port 5000
 })
-
-app.listen(5000); // Node + Express server will be on port 5000
