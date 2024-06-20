d28114917529da7493f256b55e50e9f19554d162
Almenorca <105606096+Almenorca@users.noreply.github.com>
2024-06-19 21:54:58 -0400

    Updated user.js
    - Added uniqueString and Verified parameters for verification purposes
    - Added the ability to send verification emails using a gmail account
    - Created a verify API that allows users to verify their account (Verified will be change to true upon clicking the link)

--- API/user.js
+++ API/user.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const {getClient} = require('../database');
 const router = express.Router();
+const nodemailer = require('nodemailer');
 
 // User Registration
 router.post('/register', async (req, res, next) => {
     const db = getClient().db('AlcoholDatabase')
-    const {UserId, FirstName, LastName, Username, Password, Email, Phone} = req.body //May need to be adjusted if we need to have email verification
+    const {FirstName, LastName, Username, Password, Email, Phone} = req.body //May need to be adjusted if we need to have email verification
 
     //Verification to check if username and email exists in the database
     let results = await db.collection('Users').find({ Username:Username}).toArray()
@@ -17,7 +18,6 @@ router.post('/register', async (req, res, next) => {
         return res.status(409).json({ error: 'Email already exists' });
     }
 
-
     //Used to auto-increment UserId using last register user's id
     let nextUserId
     const lastUser = await db.collection('Users').find().sort({ UserId:-1 }).limit(1).toArray()
@@ -28,35 +28,97 @@ router.post('/register', async (req, res, next) => {
         nextUserId = 1
     }
 
-    const user = {UserId:nextUserId, FirstName, LastName, Username, Password, Email, Phone}
+    const uniqueString = randString() //Used to help create a unique verification link that is attached to the user's account
+
+    const user = {UserId:nextUserId, FirstName, LastName, Username, Password, Email, Phone, Verified:false, uniqueString}
 
     await db.collection('Users').insertOne(user)
+    sendMail(Email, uniqueString)
+
     var ret = { UserId:nextUserId, FirstName:FirstName, LastName:LastName, 
-    Username:Username, Password:Password, Email:Email, Phone:Phone, Message:'User successfully registered'}
+    Username:Username, Password:Password, Email:Email, Phone:Phone, Message:"User successfully registered. Verification email has been sent."}
     res.status(200).json(ret)
 });
 
+//Generates a unique string used for the verification link
+const randString = () => {
+    const length = 10
+    let string = ''
+    for(let i = 0; i < length; i++){
+        const character = Math.floor((Math.random() * 10) + 1)
+        string += character
+    }
+
+    return string
+}
+
+const sendMail = (Email, uniqueString) => {
+    const Transport = nodemailer.createTransport({
+        service: 'gmail',
+        host: "smtp.gmail.com", //
+        port: 465,
+        secure: true,
+        auth: {
+            user: '', // Use your gmail username
+            pass: '' // Use an App Password, not your gmail password 
+                                        //(Manage Your Google Account -> Security -> 2-Step Verification -> App Passwords)
+        }
+    });
+    const mailOptions = {
+        from: 'noreply@domain.com',  //Change later   
+        to: Email,
+        subject: "Email Confirmation",
+        html: `Press <a href=http://localhost:5000/api/verify/${uniqueString}> here </a> to verify your email.` //Links to the api/verify
+    }
+
+    console.log(mailOptions)
+    Transport.sendMail(mailOptions, function(error, res){
+        if(error){
+            console.log("Email was not sent.")
+            console.log(error)
+        }
+        else{
+            console.log("Email was sent.")
+        }
+    })
+
+}
 // User Login
 router.post('/login', async (req, res, next) => {
     const db = getClient().db('AlcoholDatabase')
-    var error = ''
     const { Username, Password } = req.body
     const results = await db.collection('Users').find({ Username:Username,Password:Password}).toArray()
     var id = -1
     var fn = ''
     var ln = ''
     
-    if( results.length > 0 ) {
+    if( results.length > 0 && results[0].Verified) {
         id = results[0].UserId
         fn = results[0].FirstName
         ln = results[0].LastName
+    }
+    else if(results.length > 0 && !results[0].Verified){
+        return res.status(401).json({error:"Account has not been verified"}) 
     }
     else{
         return res.status(401).json({error:"Invalid Username or Password"})
     }
     
-    var ret = { UserId:id, FirstName:fn, LastName:ln, Message:'User successfully logged in'}
+    var ret = { UserId:id, FirstName:fn, LastName:ln, Message:"User successfully logged in"}
     res.status(200).json(ret)
+})
+
+// User Verification - Verifies user in the database when they click email link.
+router.get('/verify/:uniqueString', async(req, res) =>{
+    const db = getClient().db('AlcoholDatabase')
+    const{uniqueString} = req.params
+    const result = await db.collection('Users').updateOne({ uniqueString: uniqueString }, { $set: { Verified: true } })// Finds user using uniqueString then updates Verified to true
+    if(result){
+        res.status(200).json({message:"User has been successfully verified"})
+    }
+    else{
+        return res.status(401).json({error:"Error validating user"})
+    }
 })
 
 module.exports = router;
