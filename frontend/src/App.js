17e2d848332daf66b9297b8aa8f2b1245254b9c5
JAMuniz <jo651706@ucf.edu>
2024-06-24 14:12:54 -0400

    Addition of LoginPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
+      <LoginPage />
       <HomePage />
     </div>
   );
