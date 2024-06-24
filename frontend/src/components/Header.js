9ab1e3e41806fcd56a0a3e3a21902b03852c4567
JAMuniz <jo651706@ucf.edu>
2024-06-24 12:58:43 -0400

    Update Header.js
    Logo image updated

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,12 +5,13 @@ import '../main.css';
 function Header(){
     return(
         <div className="header">
-            <a href="../../public/index.html"><img src={logoImage} alt="Paradise Pours Logo" class="logo-img"></img></a>
+            <div>
+                <a href="../../public/index.html"><img src={logoImage} alt="Paradise Pours Logo" class="logo-img"></img></a>
+            </div>
             <div class="menu">
                 <div className="beer menu-button">Beer</div>
                 <div className="wine menu-button">Wine</div>
                 <div className="liquor menu-button">Liquor</div>
-                <div className="menu menu-button">Menu</div>
             </div>
         </div>
     );
