eba0b049990ea75a0b43adf65116d04b9e580c6f
JAMuniz <jo651706@ucf.edu>
2024-06-24 14:12:26 -0400

    Create LoginPage.js
    Login Page Prototype Creation

---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import LoginHeader from '../components/LoginHeader';
+import '../main.css';
+
+function LoginPage() {
+    return (
+        <div className="login-page">
+            <div className="loginContent"> 
+                <LoginHeader />
+                <div className="login-box">
+                    <h2>Login / Register</h2>
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" id="username" name="username" required />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id="password" name="password" required />
+                        </div>
+                            <button type="submit" className="login-button">Login</button>
+                            <button type="button" className="register-button">Register</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default LoginPage;
