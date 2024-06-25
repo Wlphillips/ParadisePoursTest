a5bbe9caeed2f8609c9e59795ee735cbaf1a2ff8
Connor34Bennett <co617187@ucf.edu>
2024-06-25 03:26:18 -0400

    Wine Page
    Starting the Wine Page, added a background and changed the accent color for the header. Could change the background if we want

---
+++ frontend/src/components/WineHeader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logoImage from "../images/Paradise_Pours_Logo_Circle.png";
+import '../main.css';
+
+function WineHeader(){
+    return(
+        <div className="wine-header">
+            <div>
+                <a href="/"><img src={logoImage} alt="Paradise Pours Logo" class="logo-img"></img></a>
+            </div>
+            <div class="wine-menu">
+            <div className="aboutus wine-menu-button">About Us</div>
+            </div>
+        </div>
+    );
+}
+
+export default WineHeader;
--- frontend/src/pages/WinePage.js
+++ frontend/src/pages/WinePage.js
@@ -1,5 +1,19 @@
-function WinePage(){
+import WineHeader from "../components/WineHeader";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
+
+function WinePage(){
+    return(
+        <div class ="wine-page">
+            <div class = "wine-content">
+                <WineHeader />
+                <div class = "wine-box">
+                    <h1>Wine</h1>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default WinePage;
