7af50302e5c1d2deb944b2f00f0a8c9c7919c0b1
Jose Neto <jo651706@ucf.edu>
2024-07-03 23:51:16 -0400

    Add link to our github and Icon to about us text

--- frontend/src/components/AboutUs.js
+++ frontend/src/components/AboutUs.js
@@ -1,11 +1,16 @@
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+
 function AboutUs(){
     return(
         <div className="about-us" id = "AU">
-          <h1>About Us</h1>
-          <p>At Paradise Pours, we aim to provide comprehensive insight into the nutritional content of any alcohol of your choice such as beer, wine, and liquor.
-            Understanding these details is crucial for making informed choices about consumption.
-            Whether you're curious about the calorie content, ABV, or other nutritional factors, our goal is to empower you with knowledge that promotes responsible and informed drinking habits.
-            Explore our resources to uncover the nutritional facts of your favorite drinks and discover how they fit into your lifestyle.</p>
+            <Link to="https://github.com/DanTySmall/Large-Project" className="link-git"><FontAwesomeIcon icon={faInfoCircle} className="about-us-icon" />About Us</Link>
+            <p>At Paradise Pours, we aim to provide comprehensive insight into the nutritional content of any alcohol of your choice such as beer, wine, and liquor.
+                Understanding these details is crucial for making informed choices about consumption.
+                Whether you're curious about the calorie content, ABV, or other nutritional factors, our goal is to empower you with knowledge that promotes responsible and informed drinking habits.
+                Explore our resources to uncover the nutritional facts of your favorite drinks and discover how they fit into your lifestyle.
+            </p>
         </div>
     );
 }
