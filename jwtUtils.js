0b509be45f473f28226057d4097fe566ab7565fb
Almenorca <105606096+Almenorca@users.noreply.github.com>
2024-07-03 20:56:21 -0400

    Reverted changes

--- jwtUtils.js
+++ jwtUtils.js
@@ -1,21 +1,8 @@
 const jwt = require('jsonwebtoken')
 JWT_SECRET = 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcxOTkyNzUyMywiaWF0IjoxNzE5OTI3NTIzfQ.TMcuZI8dhkTphWsGpyrU-T7NDN6W9vOp0yrPThkxwiQ'
-JWT_REFRESH = 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcyMDA0Njg1MSwiaWF0IjoxNzIwMDQ2ODUxfQ.u_oIzEqm4AkzujvD3cXHg56rGO5nclG2nf6R10rbEok'
 
-function generateToken(user) {
-    return jwt.sign({User: user}, JWT_SECRET, {expiresIn: '1d'})
+function generateToken(userId) {
+    return jwt.sign({User: userId}, JWT_SECRET, {expiresIn: '1d'})
 }
 
-function authUser(token){
-    if(!token){
-        console.log("token failed")
-        return
-    }
-    return jwt.verify(token, JWT_SECRET)
-}
-
-function refreshToken(){
-
-}
-
-module.exports = {generateToken, authUser}
+module.exports = {generateToken}
