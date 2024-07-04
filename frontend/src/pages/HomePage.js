96a54b4a1ee7f580d2a7c1a9b660fd8e03a39cc3
Almenorca <105606096+Almenorca@users.noreply.github.com>
2024-07-03 20:33:25 -0400

    Added a useEffect to retrieve login information
    useEffect() gets user information and stores it in local storage. We don't necessarily have to keep this setup

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from 'axios';
 import '../main.css';
 import Header from '../components/Header.js';
 import AboutUs from '../components/AboutUs.js';
 
 const HomePage = () => {
+
+  useEffect(() => {
+    const getUser = async() => {
+        try{
+            axios.defaults.withCredentials = true;
+            const response = await axios.get('http://localhost:5000/api/user')
+            console.log(response.data)
+            if(response){
+              localStorage.setItem('user', JSON.stringify(response.data))
+            }
+        }
+        catch (error){
+            console.error("Issues retrieving user")
+        }
+    }
+    getUser()
+}, [])
+
   return (
     <div className="home-page">
       <div className="content" >
