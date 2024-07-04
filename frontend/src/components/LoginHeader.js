32076766f97c896be498e8678ffe9e2380683a2a
Jose Neto <jo651706@ucf.edu>
2024-07-03 21:33:41 -0400

    add about-us icon

--- frontend/src/components/LoginHeader.js
+++ frontend/src/components/LoginHeader.js
@@ -2,7 +2,8 @@ import React from 'react';
 import logoImage from "../images/Paradise_Pours_Logo_Circle.png";
 import '../main.css';
 import { Link } from 'react-router-dom';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 function LoginHeader(){
     return(
@@ -11,7 +12,7 @@ function LoginHeader(){
                 <a href="/"><img src={logoImage} alt="Paradise Pours Logo" class="logo-img"></img></a>
             </div>
             <div class="menu">
-            <Link to = "/about+us" className="aboutus login-menu-button">About Us</Link>
+            <Link to = "/about+us" className="aboutus login-menu-button"><FontAwesomeIcon icon={faInfoCircle} className="about-us-icon" />About Us</Link>
             </div>
         </div>
     );
