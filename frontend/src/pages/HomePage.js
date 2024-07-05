c523cd1c399a4c861d8465c5de4913b69ac14e91
Almenorca <105606096+Almenorca@users.noreply.github.com>
2024-07-05 13:41:22 -0400

    Can retriever user information from backend.
    - Added a useState() to store user information
    - User information is received from the API call /user and this can be reused anywhere.

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import '../main.css';
 import Header from '../components/Header.js';
 import AboutUs from '../components/AboutUs.js';
 
 const HomePage = () => {
+  const [user, setUser] = useState('')
+  useEffect(() => {
+    const getUser = async() => {
+        try{
+            const response = await axios.get('http://localhost:5000/api/user',
+              {
+                withCredentials: true,
+              }
+            )
+            console.log(response.data)
+            if(response.status === 200){
+              console.log(response)
+              setUser(response.data.User)
+            }
+        }
+        catch (error){
+            console.error("Issues getting user")
+        }
+    }
+
+    getUser()
+}, [])
+
   return (
     <div className="home-page">
       <div className="content" >
