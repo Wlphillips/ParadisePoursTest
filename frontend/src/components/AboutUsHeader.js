ada514be34f63ff3cf514f0227db9e95f4e4e875
Jose Neto <jo651706@ucf.edu>
2024-07-07 16:53:26 -0400

    Fixed DOM properties

--- frontend/src/components/AboutUsHeader.js
+++ frontend/src/components/AboutUsHeader.js
@@ -8,10 +8,10 @@ function AboutUsHeader(){
     return(
         <div className="header">
             <div>
-                <Link to="/"><img src={logoImage} alt="Paradise Pours Logo" class="logo-img"></img></Link>
+                <Link to="/"><img src={logoImage} alt="Paradise Pours Logo" className="logo-img"></img></Link>
             </div>
-            <div class="menu">
-                <Link to="/" className="log menu-button"><i class="bi bi-arrow-left"></i>Back</Link>
+            <div className="menu">
+                <Link to="/" className="log menu-button"><i className="bi bi-arrow-left"></i>Back</Link>
             </div>
         </div>
     );
