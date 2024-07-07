48610d5ecb9c9b0b772b73b0b1d0ac1f5a05f604
Jose Neto <jo651706@ucf.edu>
2024-07-07 17:39:18 -0400

    Liquor page base creation and background image

--- frontend/src/pages/LiquorPage.js
+++ frontend/src/pages/LiquorPage.js
@@ -1,5 +1,17 @@
-function LiquorPage(){
+import LiquorHeader from "../components/LiquorHeader";
+import React, { useState, useEffect } from 'react';
 
+function LiquorPage(){
+    return(
+        <div className ="liquor-page">
+            <div className = "liquor-content">
+                <LiquorHeader />
+                <div class = "liquor-box">
+                    <h1>Liquor</h1>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default LiquorPage;
