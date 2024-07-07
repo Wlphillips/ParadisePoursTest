ebba98c3042c2d0bcd01ed23d9fbbc261e05c1f8
Jose Neto <jo651706@ucf.edu>
2024-07-07 17:38:50 -0400

    Fixed DOM properties and modified wine header to show new options

--- frontend/src/components/WineHeader.js
+++ frontend/src/components/WineHeader.js
@@ -2,16 +2,21 @@ import React from 'react';
 import logoImage from "../images/Paradise_Pours_Logo_Circle.png";
 import '../main.css';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome,faBeer,faCocktail,faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 
 function WineHeader(){
     return(
         <div className="wine-header">
             <div>
-                <a href="/homepage"><img src={logoImage} alt="Paradise Pours Logo" class="logo-img"></img></a>
+                <a href="/homepage"><img src={logoImage} alt="Paradise Pours Logo" className="logo-img"></img></a>
             </div>
-            <div class="menu">
-            <Link to = "/homepage" className="wine-menu-button"><i class="bi bi-arrow-left"></i>Back</Link>
+            <div className="menu">
+                <Link to="/homepage" className="home menu-button"><FontAwesomeIcon icon={faHome} className="home-icon" />Home</Link>
+                <Link to="/beer" className="beer menu-button"><FontAwesomeIcon icon={faBeer} className="beer-icon" />Beer</Link>
+                <Link to="/liquor" className="liquor menu-button"><FontAwesomeIcon icon={faCocktail} className="cocktail-icon" />Liquor</Link>
+                <Link to="/" className="log menu-button"><FontAwesomeIcon icon={faSignOutAlt} className="logout-icon" />Logout</Link>
             </div>
         </div>
     );
