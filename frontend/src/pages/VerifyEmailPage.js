7f3187386a1b656d5fc3c1f25ba76d8b419e5362
Wlphillips <Wlphillips.home@gmail.com>
2024-07-09 18:44:33 -0400

    Changed url to match heroku

--- frontend/src/pages/VerifyEmailPage.js
+++ frontend/src/pages/VerifyEmailPage.js
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom';
 import '../main.css';
 
 const VerifyEmailPage = () =>{
-    const app_name = 'paradisepours-3e2f83df36a7'
+    const app_name = 'paradisepours-85b61313006b'
     function buildPath(route)
     {
         if (process.env.NODE_ENV === 'production')
